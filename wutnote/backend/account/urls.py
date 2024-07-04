6c7a86fa47e63b7ab462ed9389f2aa51f7199ad8
Aminlex <3043146472@qq.com>
2024-07-04 09:33:29 +0800

    Delete wutnote/backend directory
    传错了

--- wutnote/backend/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login_view, protected_view,my_view
-urlpatterns = [
-   path('login/', login_view),
-   path('protected_view/',protected_view),
-
-   path('my/',my_view),
-]
--- wutnote/backend/note/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import notelist, note_detail
-urlpatterns = [
-   path('notelist/', notelist), # 所有笔记列表
-   path('<int:id>/', note_detail) # 根据id查询笔记详情
-]
